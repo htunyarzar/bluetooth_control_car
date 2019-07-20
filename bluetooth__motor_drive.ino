75194927845034801098c08ed72e48abd2dfe144
htunyarzar <53105601+htunyarzar@users.noreply.github.com>
2019-07-20 10:01:04 +0630

    first_commit

---
+++ bluetooth__motor_drive.ino
@@ -0,0 +1,85 @@
+char drive;
+
+void setup() {
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  drive = Serial.read();
+
+  if (drive == 'f')
+  {
+    forward();
+  }
+
+  if (drive == 'b')
+  {
+    backward();
+  }
+
+  if (drive == 'r')
+  {
+    right();
+  }
+
+  if (drive == 'l')
+  {
+    left();
+  }
+
+  if (drive == 's')
+  {
+    stop_motor();
+  }
+
+}
+
+void forward()
+{
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
+}
+
+void backward()
+{
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+}
+
+void right()
+{
+  digitalWrite(5, HIGH);
+  digitalWrite(6, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+}
+
+void left()
+{
+  digitalWrite(5, LOW);
+  digitalWrite(6, HIGH);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
+}
+
+void stop_motor()
+{
+  digitalWrite(5, LOW);
+  digitalWrite(6, LOW);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+}
+
+
+
+
+
